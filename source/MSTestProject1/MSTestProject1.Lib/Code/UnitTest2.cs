2b5f1f8675c4bdaaa30adfdaa1f350d66cd695f2
MinexAutomation <minex.automation@gmail.com>
2019-06-28 11:38:11 -0400

    Added integer instance and empty string instance equality.

--- source/MSTestProject1/MSTestProject1.Lib/Code/UnitTest2.cs
+++ source/MSTestProject1/MSTestProject1.Lib/Code/UnitTest2.cs
@@ -8,10 +8,13 @@ namespace MSTestProject1.Lib
     [TestClass]
     public class UnitTest2
     {
+        /// <summary>
+        /// Test of empty string instance equality.
+        /// </summary>
         [TestMethod]
         public void TestMethod2()
         {
-
+            Assert.AreEqual(String.Empty, String.Empty);
         }
     }
 }
--- source/MSTestProject1/MSTestProject1/Code/UnitTest1.cs
+++ source/MSTestProject1/MSTestProject1/Code/UnitTest1.cs
@@ -8,9 +8,13 @@ namespace MSTestProject1
     [TestClass]
     public class UnitTest1
     {
+        /// <summary>
+        /// Test of integer instance equality.
+        /// </summary>
         [TestMethod]
         public void TestMethod1()
         {
+            Assert.AreEqual(1, 1);
         }
     }
 }
