3c342f93ac5d1de012db3e399901c9ce96062c4c
MinexAutomation <minex.automation@gmail.com>
2019-06-28 12:48:21 -0400

    MSTestProject2 - Added MSTest.TestsLibrary package reference.

--- source/MSTestProject2/MSTestProject2/Code/UnitTest1.cs
+++ source/MSTestProject2/MSTestProject2/Code/UnitTest1.cs
@@ -1,13 +1,20 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using MSTestProject1.Lib;
+
+
 namespace MSTestProject2
 {
     [TestClass]
     public class UnitTest1
     {
+        /// <summary>
+        /// Test of double instance equality.
+        /// </summary>
         [TestMethod]
         public void TestMethod1()
         {
+            Assert.AreEqual(2.0, 2.0);
         }
     }
 }
