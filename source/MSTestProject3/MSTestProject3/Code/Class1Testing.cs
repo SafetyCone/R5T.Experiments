b5d3c7f53c6122355913af10599806fab73fedbc
MinexAutomation <minex.automation@gmail.com>
2019-06-28 12:29:56 -0400

    MSTestProject3 - Intial commit.

---
+++ source/MSTestProject3/MSTestProject3/Code/Class1Testing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ClassLibrary1;
+using ClassLibrary1.Tests;
+
+
+namespace MSTestProject3
+{
+    [TestClass]
+    public class Class1Testing : Class1Tests
+    {
+        public Class1Testing()
+            : base(new Class1 { Value = Class1.ProvideValue() })
+        {
+        }
+    }
+}
