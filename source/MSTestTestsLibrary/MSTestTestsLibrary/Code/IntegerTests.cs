4a3610609749704c7a55cf92d92ea9346201cecd
MinexAutomation <minex.automation@gmail.com>
2019-06-28 13:08:14 -0400

    MSTestTestsLibrary - Initial commit.

---
+++ source/MSTestTestsLibrary/MSTestTestsLibrary/Code/IntegerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace MSTestTestsLibrary
+{
+    [TestClass]
+    public class IntegerTests
+    {
+        /// <summary>
+        /// Tests integer instances for equality.
+        /// </summary>
+        [TestMethod]
+        public void IntegerInstanceEquality()
+        {
+            Assert.AreEqual(3, 3);
+        }
+    }
+}
