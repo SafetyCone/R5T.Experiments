59aaeef9c0d9174c4ef100dffbaa9f34abd39751
MinexAutomation <minex.automation@gmail.com>
2019-06-28 12:45:06 -0400

    MSTest.TestsLibrary - Initial commit.

---
+++ source/MSTest.TestsLibrary/MSTest.TestsLibrary.Testing/Code/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTest.TestsLibrary.Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ source/MSTest.TestsLibrary/MSTest.TestsLibrary/Code/DoubleTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace MSTest.TestsLibrary
+{
+    [TestClass]
+    public class DoubleTests
+    {
+        /// <summary>
+        /// Tests the equality of two double instances.
+        /// </summary>
+        [TestMethod]
+        public void InstanceEquality()
+        {
+            Assert.AreEqual(2.0, 2.0);
+        }
+    }
+}
