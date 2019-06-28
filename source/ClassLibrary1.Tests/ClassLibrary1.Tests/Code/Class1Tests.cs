2d08546c37d7ebcf65c58e76e4a58fed2e68a549
MinexAutomation <minex.automation@gmail.com>
2019-06-28 12:22:46 -0400

    ClassLibrary1.Tests - Initial commit, including tests base-class for IClass1 implementations.

---
+++ source/ClassLibrary1.Tests/ClassLibrary1.Tests/Code/Class1Tests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace ClassLibrary1.Tests
+{
+    [TestClass]
+    public abstract class Class1Tests
+    {
+        private IClass1 Class1 { get; }
+
+
+        public Class1Tests(IClass1 class1)
+        {
+            this.Class1 = class1;
+        }
+
+
+        /// <summary>
+        /// Test that the provided value is correct.
+        /// </summary>
+        [TestMethod]
+        public void ProvidedValueIsCorrect()
+        {
+            var value = this.Class1.Value;
+
+            var expected = "The Value";
+
+            Assert.AreEqual(expected, value);
+        }
+    }
+}
