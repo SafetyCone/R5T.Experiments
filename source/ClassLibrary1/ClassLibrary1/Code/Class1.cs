f19bb7b3340605df7055edf79cb2cf884d7c26df
MinexAutomation <minex.automation@gmail.com>
2019-06-28 11:16:01 -0400

    ClassLibrary1 - Initial commit. Includes Class1 basic class functionality.

---
+++ source/ClassLibrary1/ClassLibrary1/Code/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public const string ValueValue = "The Value";
+
+
+        #region Static
+
+        public static string ProvideValue()
+        {
+            return Class1.ValueValue;
+        }
+
+        #endregion
+
+
+        public string Value { get; set; }
+
+
+        public Class1()
+        {
+        }
+
+        public Class1(string value)
+        {
+            this.Value = value;
+        }
+    }
+}
